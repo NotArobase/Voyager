5038bd1d0058a27f70f24e22dab366b355690799
NotArobase <maximegrenet@gmail.com>
2025-02-17 14:49:22 +0100

    fixs

--- pipeline/datamine/datamine_roles.py
+++ pipeline/datamine/datamine_roles.py
@@ -1,16 +1,16 @@
 import importlib.util
 from typing import Any, Optional
 from pipeline.base import Stage, ResultMap
-from config import MainConfig
+from config import DatamineConfig
 
-class DatamineStage(Stage[Any, MainConfig]):
+class DatamineStage(Stage[Any, DatamineConfig]):
     """
     Stage générique qui délègue l'algorithme à un script externe.
     
     Optionnellement (ou pas), le script peut définir une fonction `store_results` pour gérer le stockage.
     """
 
-    def run(self, algo_script_path: str, needs_data: bool, data: Optional[Any] = None) -> ResultMap[Any]:
+    def run(self) -> ResultMap[Any]:
         """
 
         :param algo_script_path: Chemin vers le script Python de l'algorithme.
@@ -19,7 +19,7 @@ class DatamineStage(Stage[Any, MainConfig]):
         :return: Un ResultMap contenant le résultat de l'algorithme.
         """
     
-        spec = importlib.util.spec_from_file_location("external_algo", algo_script_path)
+        spec = importlib.util.spec_from_file_location("external_algo", self.config.path)
         external_algo = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(external_algo)
 
@@ -27,10 +27,8 @@ class DatamineStage(Stage[Any, MainConfig]):
             raise AttributeError("Le script de l'algorithme doit contenir une fonction 'run_algo'.")
 
         # Exécuter l'algorithme en fonction du paramètre needs_data 
-        if needs_data:
-            if data is None:
-                raise ValueError("L'algorithme nécessite des données, mais aucune donnée n'a été fournie.")
-            result = external_algo.run_algo(data, self.config.output_directory)
+        if self.config.options:
+            result = external_algo.run_algo(self.config.output_directory, self.config.options)
         else:
             result = external_algo.run_algo(self.config.output_directory)
 
